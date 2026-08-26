e5acf7a1cf9d1fdf83998f3789fc2b26fd08c40f
iliyaPour <iliya.pourzanjani6@gmail.com>
2026-08-26 22:38:29 +0330

    Added Status Card

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,31 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('tasks', TaskController::class)->only(['index', 'store', 'update', 'destroy']);
 });
 
+Rout::get('/dashboard', function () {
+    $lists = \App\Models\TodoList::query()
+    ->withCount(['tasks', 'tasks as completed_tasks_count' => function ($q) {
+        $q->where('completed', true);
+    }])
+    ->latest()
+    ->get();
+
+    $recentTasks = \App\Models\Task::query()
+    ->with('list::id,name,color')
+    ->latest()
+    ->take(10)
+    ->get();
+
+    $totalTasks = \App\Models\Task::count();
+    $completedTasks = \App\Models\Task::where('completed', true)->count();
+    $pendingTasks = \App\Models\Task::where('completed', false)->count();
+
+    return Inertia::render('Dashboard', [
+        'lists' => $lists,
+        'recentTasks' => $recentTasks,
+        'totalTasks' => $totalTasks,
+        'completedTasks' => $completedTasks,
+        'pendingTasks' => $pendingTasks,
+    ]);
+})-middleware(['auth', 'verified'])->name('dashboard');
+
 require __DIR__.'/settings.php';
